c6627d0527e0620e164079e77f0b9f66ea3ff4bc
earthctzn <earthctzn1@gmail.com>
2020-08-10 14:37:55 -0400

    Imports Grid and adds preliminary layout to App.js.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 import CounterInput from './counterInput'
+import { Grid } from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <CounterInput />
-      </header>
-    </div>
+    <Grid container direction='column'>
+      <Grid  item container>
+        This is where the appBar content will be displayed
+      </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2}/>
+        <Grid  item xs={12} sm={8}>
+          This is where the CounterInput component will be displayed
+        </Grid>
+        <Grid item xs={0} sm={2}/>
+      </Grid>
+
+    </Grid>
   );
 }
 
