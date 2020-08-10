9703e7cc32a47685fd61a9ae45f792369b933d32
earthctzn <earthctzn1@gmail.com>
2020-08-10 18:33:54 -0400

    Updates syntax for legibility in App.js.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { Grid } from '@material-ui/core'
 
 function App() {
   return (
+    
     <Grid container direction='column'>
-      <Grid  item container>
-        This is where the appBar content will be displayed
-      </Grid>
-      <Grid item container>
+      
+      <Grid container item>This is where the appBar content will be displayed</Grid>
+
+      <Grid container item>
         <Grid item xs={0} sm={2}/>
-        <Grid  item xs={12} sm={8}>
+        <Grid item xs={12} sm={8}>
           This is where the CounterInput component will be displayed
         </Grid>
         <Grid item xs={0} sm={2}/>
