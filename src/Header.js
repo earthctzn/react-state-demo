178e233ddb90aa730bf2a30dcc6345d55816c717
earthctzn <earthctzn1@gmail.com>
2020-08-11 00:06:53 -0400

    Adds makeStyles hook generator to style  Header component.

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
 import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
 
+const useStyles = makeStyles(() => ({
+    navStyle: {
+        flex: 1
+    }
+}))
+
 const Header = () => {
+    const classes = useStyles()
     return(
         <AppBar position='static'>
             <Toolbar>
-                <Typography>
+                <Typography className={classes.navStyle}>
                     React Counter with Material-UI
                 </Typography>
-                <AccountCircleRoundedIcon/>
-            </Toolbar>
-            
+                <AccountCircleRoundedIcon />
+            </Toolbar> 
         </AppBar>
     )
 }
